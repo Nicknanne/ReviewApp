0e2224288958daf70f64712c9c57ff424980980a
Nicknanne <NicknameU@yandex.ru>
2025-06-10 21:00:25 +0300

    clean up gitignore

--- ReviewApp/MainPage.xaml.cs
+++ ReviewApp/MainPage.xaml.cs
@@ -1,16 +1,22 @@
-﻿namespace ReviewApp
+﻿using System.Diagnostics;
+
+namespace ReviewApp
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        private readonly List<int> _myList;
 
-        public MainPage()
+        public MainPage(List<int> myList)
         {
+            _myList = myList;
             InitializeComponent();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            Debug.WriteLine(string.Join(", ", _myList));
+
             count++;
 
             if (count == 1)
